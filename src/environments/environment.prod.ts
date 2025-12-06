38ccac45d2a89a94f8d21261853ee3969cd2a754
Aditya Pandya <lazzyspider@Adityas-Mac-mini.local>
2025-12-06 15:47:58 +0530

    for prod changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: true,
     recaptchaSiteKey: '6LdXr_QrAAAAAPVnHV-blIYKkZJlkcRBPoREIBHr', // Keep the same or use a prod key if they have one
-    baseUrl: 'https://acu-backend.onrender.com/' // TODO: Replace with your actual production backend URL
+    baseUrl: 'https://acu-backend.onrender.com' // TODO: Replace with your actual production backend URL
 };
